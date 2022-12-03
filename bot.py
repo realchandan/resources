f6ae5aac6d2b37c541f901c99e0536834dac3dcf
realchandan <contact@chandan.cc>
2022-12-03 15:57:31 +0530

    old way

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
-from dotenv import load_dotenv
+from os import environ
 
+from discord import Intents, Message
+from discord.ext.commands import Bot
+from dotenv import load_dotenv
 
 load_dotenv()
 
-from discord import Client, Intents, Message
+intents = Intents.all()
 
-from os import environ
-
-bot = Client(intents=Intents.all())
+bot = Bot(command_prefix="!", intents=intents)
 
 
 @bot.event
@@ -15,12 +16,18 @@ async def on_ready():
     print("The bot is ready")
 
 
-@bot.event
-async def on_message(message: Message):
-    if message.author == bot.user:
-        return
+@bot.command(name="hello")
+async def hello(message: Message):
     print(message)
-    await message.reply("Hello, welcome to our server!")
+    await message.reply(f"Hello {message.author.name}!")
+
+
+# @bot.event
+# async def on_message(message: Message):
+#     if message.author == bot.user:
+#         return
+#     print(message)
+#     await message.reply("Hello, welcome to our server!")
 
 
 bot.run(environ.get("BOT_TOKEN"))
