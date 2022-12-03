90971bf714738ec1873d96d5a9cc1a3cb0ef0b0b
realchandan <contact@chandan.cc>
2022-12-03 16:34:18 +0530

    basic slash commands

--- bot.py
+++ bot.py
@@ -1,33 +1,27 @@
 from os import environ
 
-from discord import Intents, Message
+from discord import Intents, Interaction, Object
 from discord.ext.commands import Bot
 from dotenv import load_dotenv
 
 load_dotenv()
 
-intents = Intents.all()
+intents = Intents.default()
 
 bot = Bot(command_prefix="!", intents=intents)
+
+guild = Object(id=1043859468447911976)
 
 
 @bot.event
 async def on_ready():
     print("The bot is ready")
+    await bot.tree.sync(guild=guild)
 
 
-@bot.command(name="hello")
-async def hello(message: Message):
-    print(message)
-    await message.reply(f"Hello {message.author.name}!")
-
-
-# @bot.event
-# async def on_message(message: Message):
-#     if message.author == bot.user:
-#         return
-#     print(message)
-#     await message.reply("Hello, welcome to our server!")
+@bot.tree.command(name="hello", guild=guild)
+async def hello(interaction: Interaction):
+    await interaction.response.send_message(f"Hello {interaction.user.name}!")
 
 
 bot.run(environ.get("BOT_TOKEN"))
